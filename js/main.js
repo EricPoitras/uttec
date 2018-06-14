dfece5ac54370be5fa9eb64c391f338cc570e0e0
EricPoitras <EricPoitras@users.noreply.github.com>
2018-06-13 23:46:46 -0600

    Update the js document to dynamically redraw histogram

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 google.charts.load('current', {packages: ['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawAxisTickColors);
 
+var tigercount = 0;
+var hippocount = 0;
+
 function drawAxisTickColors() {
       var data = google.visualization.arrayToDataTable([
          ['Group', 'Count', { role: 'style' }],
-         ['Tiger', 25, 'red'],
-         ['Hippo', 10, 'blue'],            
+         ['Tiger', tigercount, 'red'],
+         ['Hippo', hippocount, 'blue'],            
       ]);
     
       var options = {
@@ -16,3 +19,17 @@ function drawAxisTickColors() {
       var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
       chart.draw(data,options);
     }
+
+$(document).ready(function(){
+    console.log("Document load event");
+    
+    $("#tigerbutton").click(function(){
+        tigercount++;
+        drawAxisTickColors();
+    });
+    
+    $("#hippobutton").click(function(){
+        hippocount++;
+        drawAxisTickColors();
+    });
+})
